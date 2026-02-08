1f9cdf7bc420d7535d68e011bffe10f02684b144
Agustin Sanchez <agustinsanchezp123@gmail.com>
2026-02-08 20:06:13 -0300

    comienzo creacion de estructura de funciones internas

--- internal/core/service.go
+++ internal/core/service.go
@@ -1,1 +1,26 @@
 package internal
+
+
+
+
+//const blacklist 
+
+const maxSize int = 800
+
+
+// verifica que el tamaño del input del usuario sea menor a 'maxSize'
+func ValidateInputSize(input string) bool {
+
+}
+
+// verifica el formato de la url ('http://' o 'https://')
+func ValidateInputFormat(input string) bool {
+
+}
+
+// verifica que la url no este dentro de la backlist
+func CheckBlacklist(input string) bool {
+
+}
+
+

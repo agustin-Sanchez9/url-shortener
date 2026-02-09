f32dbe0b39913abfe53096635bae804bb72e6ca6
Agustin Sanchez <agustinsanchezp123@gmail.com>
2026-02-09 16:39:45 -0300

    primera versio de servicios, revisar validar formato

--- internal/core/service.go
+++ internal/core/service.go
@@ -1,9 +1,11 @@
 package internal
 
+import "strings"
 
 
 
-//const blacklist 
+
+var blacklist = [1]string{"domain.com"}
 
 const maxSize int = 800
 
@@ -11,16 +13,34 @@ const maxSize int = 800
 // verifica que el tamaño del input del usuario sea menor a 'maxSize'
 func ValidateInputSize(input string) bool {
 
+	if len(input) > maxSize {
+		return false
+	}
+	return true
 }
 
-// verifica el formato de la url ('http://' o 'https://')
+// verifica si el formato de la url es valido ('http://' o 'https://')
 func ValidateInputFormat(input string) bool {
 
+	if !strings.Contains(input, "http://") {
+		return false
+	}
+	if !strings.Contains(input, "https://") {
+		return false
+	}
+	return true
 }
 
 // verifica que la url no este dentro de la backlist
 func CheckBlacklist(input string) bool {
 
+	for i := 0; i < len(blacklist); i++ {
+		if !strings.Contains(input,blacklist[i]) {
+			return false
+		}
+	}
+
+	return true
 }
 
 
